5ecabbf821172392d285b6179863e668f20133fe
Robert Conrad <robert.conrad@ieee.org>
2023-05-13 22:16:43 -0400

    Cleaned Main Page

---
+++ lib/enviroment.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart' show ColorScheme, Colors;
+
+const String APP_NAME = "Dungeon Maker (WIP)";
+
+ColorScheme APP_THEME = ColorScheme.fromSeed(seedColor: Colors.red);
